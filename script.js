45a29aa070665032621d40babf70269a52390de5
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-03-30 09:39:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const descriptionElement = document.getElementById('description');
 const humidityElement = document.getElementById('humidity');
 const windElement = document.getElementById('wind');
 
-const apiKey = '41bf6dd422e50996afb0381d44f47abb'; 
+const apiKey = '41bf6dd422e50996afb0381d44f47abb';
 async function getWeatherData(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     try {
         const response = await fetch(url);
         const data = await response.json();
